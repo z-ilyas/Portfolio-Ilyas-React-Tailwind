f98cdd898263477d1485bd628a24a9467cd1d1bd
Ilyas Mohamed <zswaleh@ymail.com>
2023-07-02 19:46:37 -0500

    experience changed

--- src/components/resume/Education.js
+++ src/components/resume/Education.js
@@ -20,27 +20,18 @@ const Education = () => {
             title="Full Stack Software Engineer"
             subTitle="Prime Digital Academy in Minneapolis,Minnesota (Febuary 2023 - July 2023)"
             result="Graduated"
-            des=" I have honed my skills in JavaScript, HTML, CSS, JQuery, React, Node.js, Redux, Saga, PostgreSQL, and Git, enabling me to tackle complex projects and develop robust applications."
+            des=" I have honed my skills in JavaScript, HTML, CSS, JQuery, React, Node.js, Redux, Saga, PostgreSQL, and Git, enabling me to tackle complex projects and develop robust applications. I recently completed an intensive full-stack software engineering program at Prime Digital Academy, where I gained practical experience by working on real-world client projects. One of my notable achievements during the program was developing a full-stack React application called Exercise Journal, which allows users to track exercises and visualize their progress."
           />
-
-        </div>
-      </div>
-      {/* part Two */}
-
-      <div>
-        <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
-          <p className="text-sm text-designColor tracking-[4px]">2020 - 2023</p>
-          <h2 className="text-3xl md:text-4xl font-bold">Certificates</h2>
-        </div>
-        <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
-          <ResumeCard
+            <ResumeCard
             title="Bachelor of Science in Business Management and a Minor in Economics"
             subTitle="St Cloud State University (SCSU) in St. Cloud,MN, (2016 - 2021)"
             result="Graduated"
-            des="I gained a comprehensive knowledge of various aspects of businesses. This foundation equipped me with the skills necessary to contribute to the overall success of an organization."
+            des="I completed my Bachelor of Science in Business Management with a Minor in Economics, which provided me with a well-rounded understanding of the principles and practices of the business world.
+            During my studies in Business Management, I gained a comprehensive knowledge of various aspects of business operations, including marketing, finance, human resources, and strategic planning. This foundation equipped me with the skills necessary to analyze business challenges, make informed decisions, and contribute to the overall success of an organization."
           />
         </div>
       </div>
+
     </motion.div>
   );
 }
--- src/components/resume/Experience.js
+++ src/components/resume/Experience.js
@@ -12,55 +12,30 @@ const Experience = () => {
       <div>
         <div className="flex flex-col gap-4">
           <p className="text-sm text-designColor tracking-[4px]">2023 - 20XX</p>
-          <h2 className="text-4xl font-bold">Job Experience</h2>
+          <h2 className="text-4xl font-bold">Job Experiences</h2>
         </div>
         <div className="mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
-            title="Sr. Software Engineer"
-            subTitle="Google - (20XX - Present) [Goal in my bucketlist]"
-            result="USA"
-            des="Google's hiring process is an important part of our culture. Googlers care deeply about their teams and the people who make them up."
+            title="Full-Stack Software Engineer Student "
+            subTitle="Prime Digital Academy (February 2023 - July 2023)"
+            result="Remote"
+            des=" I have honed my skills in JavaScript, HTML, CSS, JQuery, React, Node.js, Redux, Saga, PostgreSQL, and Git, enabling me to tackle complex projects and develop robust applications. I recently completed an intensive full-stack software engineering program at Prime Digital Academy, where I gained practical experience by working on real-world client projects. One of my notable achievements during the program was developing a full-stack React application called Exercise Journal, which allows users to track exercises and visualize their progress."
           />
           <ResumeCard
-            title="Web Developer & Trainer"
-            subTitle="Apple Developer Team - (20XX - 20XX) [Goal in my bucketlist]"
-            result="USA"
-            des="A popular destination with a growing number of highly qualified homegrown graduates, it's true that securing a role in USA isn't easy."
+            title="Property Manager"
+            subTitle="Timberland Partners (April 2022 - December 2022)"
+            result="St Cloud, MN"
+            des="In this position I Addressed resident complaints, concerns, and requests promptly and professionally. I managed community rental rates to optimize income and prepared annual budgets. I also implemented self-created marketing strategies and initiatives. I Ensured the community's appearance was well-maintained and reported necessary maintenance. I Reviewed applications, prepared leases, and completed the required paperwork"
           />
           <ResumeCard
-            title="Data Analyst Intern"
-            subTitle="Xemi - (5/6/2023 - 5/9/2023)  [3 months]"
-            result="Mumbai, Maharashtra"
-            des="Writing Web Scrapping codes to extract required data for website’s database and training ML models. Mode of work: Hybrid."
+            title="Bilingual Staffing Specialist"
+            subTitle="Doherty Staffing Specialist (January 2022 - April 2022)"
+            result="St Cloud, MN"
+            des="Recruited, interviewed, and placed qualified candidates in various positions. I Coached candidates and employees on interview skills, attendance, attitude, and workplace conduct. I also Handled daily office tasks, including paperwork, phone calls, and applicant interactions"
           />
         </div>
       </div>
-      <div>
-        <div className="flex flex-col gap-4">
-          <p className="text-sm text-designColor tracking-[4px]">20XX - 20XX</p>
-          <h2 className="text-4xl font-bold">Trainer Experience</h2>
-        </div>
-        <div className="mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
-          <ResumeCard
-            title="ML Instructor"
-            subTitle="ABC Platform (20XX - 20XX)"
-            result="UK"
-            des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora itaque accusamus corporis dolor debitis natus sapiente, voluptates possimus minima totam!"
-          />
-          <ResumeCard
-            title="Web Developer and Instructor"
-            subTitle="ABC King's College (20XX - 20XX)"
-            result="CANADA"
-            des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora itaque accusamus corporis dolor debitis natus sapiente, voluptates possimus minima totam!"
-          />
-          <ResumeCard
-            title="App Developer"
-            subTitle="ABC Company (20XX - 20XX)"
-            result="India"
-            des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora itaque accusamus corporis dolor debitis natus sapiente, voluptates possimus minima totam!"
-          />
-        </div>
-      </div>
+
     </motion.div>
   );
 };
